242c4011f0428f1c8782f3625303ac48b163a346
HvrI1 <hvri1@qq.com>
2024-12-09 09:18:13 +0800

    项目搭建初始化

---
+++ mpc_blogspot-back_end/src/main/java/com/mpc/mpc_blogspotback_end/MpcBlogspotBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mpc.mpc_blogspotback_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpcBlogspotBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpcBlogspotBackEndApplication.class, args);
+    }
+
+}
